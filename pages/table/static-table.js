639ead5d1674a5498569dc8e7b96495966fc89d0
elainechan <elaine.chan@outlook.com>
2018-08-25 01:01:07 -0400

    Select column on <th> click.

--- pages/table/static-table.js
+++ pages/table/static-table.js
@@ -20,6 +20,7 @@ d3.csv("./countries.csv").then((data) => {
 	var	columnNames = d3.keys(data[0]); // header values
 	var	thead = table.append("thead"); // append <thead>
 	var	tbody = table.append("tbody"); // append <tbody>
+	var sortAscending = true; // set initial sort to ascending for toggling later
 
 	// append the header row
 	thead.append("tr") // append <tr> to <thead>
@@ -29,7 +30,18 @@ d3.csv("./countries.csv").then((data) => {
 		.data(columnNames) // bind header values data
 		.enter()
 		.append("th") // append same number of <th>'s as number of rows in data 
-		.text(columnNames => columnNames);
+		.text(columnNames => columnNames)
+		.on('click', data => {
+			if (sortAscending) {
+				bodyRows.sort((a,b) => b[data] < a[data]);
+				sortAscending = false;
+			} else {
+				bodyRows.sort((a,b) => b[data] > a[data]);
+				sortAscending = true;
+			}
+		});
+	// set <th> key sequence
+	document.querySelectorAll('th').forEach((item, i) => item.setAttribute('key', i));
 
 	// create a row for each object in the data
 	var bodyRows = tbody.selectAll("tr") // create rows
@@ -37,17 +49,16 @@ d3.csv("./countries.csv").then((data) => {
 		.enter()
 		.append("tr") // append same number of <tr>'s as number of rows in data
 		.attr("class", "body") // add class name
-		.attr('class', 'row');
+		.attr('class', 'row')
 
-	// create a cell in each row for each column
-	var cells = bodyRows.selectAll("td") // create cells
+	bodyRows.selectAll("td") // create cells
 		.data(row => {
 			return columnNames.map(col => {
 				return {col: col, value: row[col]} // each datapoint
 			})
 		})
 		.enter()
-		.append("td")
+		.append("td") // append cells
 		.attr("style", "font-family: 'Lato'")
 		.html((d) => {
 			if ($.isNumeric(d.value)) {//if number is numeric, format with thousands seporator
@@ -56,11 +67,26 @@ d3.csv("./countries.csv").then((data) => {
 				return d.value; // return data value
 			};
 		});
-
+	
 	function formatMoney(n) {
 		return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
 	};
+	
+	function selectCol(x) { // select column
+		return $(`table tr > td:nth-child(${x}), table tr > th:nth-child(${x})`);
+	}
 
+	function whichColSelected(target) { // find which column is selected through nthchild index
+		return Number(target.getAttribute('key')) + 1;
+	}
+
+	function clickToHideCol(e) {
+		$('th').on('click', (e) => {
+			let n = whichColSelected(e.target);
+			$(`table tr > td:nth-child(${n}), table tr > th:nth-child(${n})`).hide();
+		});
+	}
+	clickToHideCol();
 	// Grey alternate rows
 	d3.selectAll(".row")
 		.style("background", (row, i) => {
@@ -74,9 +100,8 @@ d3.csv("./countries.csv").then((data) => {
 		.attr('href', 'https://www.kaggle.com/fernandol/countries-of-the-world/version/1#')
 		.text('Data source');	
 });
-
 	/*
 	TODO
-	set column names into thead
 	make thead clickable
+	sort table by column value
 	*/
