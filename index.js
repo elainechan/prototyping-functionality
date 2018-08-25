d6185da0b9f8c53627286785a8bc41c5a73176d3
elainechan <elaine.chan@outlook.com>
2018-08-25 01:46:04 -0400

    Set up simple home page for github pages.

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ d3.text('./README.md').then(data => {
 	$('body')
 		.prepend(mdHTML.toString()); // prepend html to top of body
 });
+
+var header = document.querySelector('header');
+var link = document.createElement('a')
+var textNode = 'Table'
+header.appendChild(link)
+	.setAttribute('href', './pages/table')
+	.appendChild(textNode);
