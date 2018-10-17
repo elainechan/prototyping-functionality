5ae19a11fe3de35c073dc53169ec72db9f35ed6a
elainechan <elaine.chan@outlook.com>
2018-10-17 14:32:06 -0400

    Renders MTA stops on browser.

--- circuit/circuit.controller.js
+++ circuit/circuit.controller.js
@@ -37,33 +37,3 @@ exports.getMtaRoutes = (req, res) => {
   console.log(`getMtaRoutes: ${JSON.stringify(routes)}`);
   return routes;
 }
-
-/* data model
-"elements" : {
-    "nodes" : [ {
-      "data" : {
-        "id" : "8220",
-        "station_name" : "京成高砂",
-        "close_ymd" : "",
-        "lat" : 35.750932,
-        "lon" : 139.866875,
-        "post" : "",
-        "e_status" : 0,
-        "SUID" : 8220,
-        "station_g_cd" : 2300110,
-        "add" : "東京都葛飾区高砂五丁目28-1",
-        "line_cd" : 99340,
-        "selected" : false,
-        "open_ymd" : "",
-        "name" : "9934001",
-        "pref_name" : "東京都",
-        "shared_name" : "9934001",
-        "y" : -357509.32,
-        "x" : 1398668.75
-      },
-      "position" : {
-        "x" : 1398668.75,
-        "y" : -357509.32
-      },
-      "selected" : false
-*/
