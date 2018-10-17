1524ac3134beb48a6b28d9858d80a3844b0cb39a
elainechan <elaine.chan@outlook.com>
2018-10-17 00:59:14 -0400

    Prepare MTA data for Cytoscape consumption.

--- public/circuit.js
+++ public/circuit.js
@@ -10,19 +10,111 @@ function handleCircuitButton() {
 	}
 }
 
-const cyData = {
-	'elements': {
-		'nodes': [
-		]
-	}
-}
-
+// define nodes using `stops`
+const nodes = [];
 function getStops() {
 	d3.csv('https://raw.githubusercontent.com/elainechan/prototyping-functionality/master/mtaStops.csv', data => {
-		debugger
 		console.log(`getStops: ${JSON.stringify(data)}`);
+		data.map((val,i) => {
+			let entry = {
+				data: {
+					id: 'n' + (i+1).toString(),
+					position: {
+						x: Number(val.station_lon) * 10000,
+						y: Number(val.station_lat) * -10000
+					},
+					selected: false,
+					selectable: true
+				}
+			};
+			nodes.push(entry);
+		});
+		return nodes;
 	})
 }
+console.log(nodes);
+
+function getRoutes() {
+	d3.csv('https://raw.githubusercontent.com/elainechan/prototyping-functionality/master/mtaRoutes.csv', data => {
+		console.log(`getRoutes: ${JSON.stringify(data)}`);
+	})
+}
+// define edges using `edges`
+const edges = [];
+function getEdges() {
+	d3.csv('https://raw.githubusercontent.com/elainechan/prototyping-functionality/master/mtaEdges.csv', data => {
+		console.log(`getEdges: ${JSON.stringify(data)}`);
+		data.map((val, i) => {
+			let entry = {
+				data: {
+					id: 'e' + (i+1).toString(),
+					source: 'n' + val.source_id,
+					target: 'n' + val.target_id,
+				}
+			};
+			edges.push(entry);
+		});
+		return edges;
+	})
+}
+console.log(edges);
+
+cytoscape({
+  container: document.getElementById('cytoscape-circuit'),
+  elements: [
+    { // node n1
+      group: 'nodes', 
+      data: {
+        id: 'n1',
+        parent: 'nparent',
+      },
+      scratch: {
+        _foo: 'bar'
+      },
+      position: { 
+        x: 100,
+        y: 100
+      },
+      selected: false, 
+      selectable: true,
+      locked: false, 
+      grabbable: true, 
+      classes: 'foo bar'
+    },
+    { // node n2
+      data: { id: 'n2' },
+      renderedPosition: { x: 200, y: 200 }
+    },
+    { // node n3
+      data: { id: 'n3', parent: 'nparent' },
+      position: { x: 123, y: 234 }
+    },
+    { // node nparent
+      data: { id: 'nparent', position: { x: 200, y: 100 } }
+    },
+    { // edge e1
+      data: {
+        id: 'e1',
+        source: 'n1',
+        target: 'n2'
+      }
+    }
+  ],
+  layout: {
+    name: 'preset'
+  },
+  style: [
+    {
+      selector: 'node',
+      style: {
+        'content': 'data(id)'
+      }
+    }
+  ]
+});
+
+// define colors of edges using `routes`
+
 /*
   [
 		{
@@ -33,7 +125,12 @@ function getStops() {
 			"station_name":"238 St","station_lat":"40.884667","station_lon":"-73.90087"}
 	]
 */
-
+const cyData = {
+	'elements': {
+		'nodes': [
+		]
+	}
+}
 const graphData = fetch('/cdata')
 	.then(res => res.json())
 	.then(data => {
@@ -51,155 +148,7 @@ const graphData = fetch('/cdata')
 		console.log(`cyData: ${JSON.stringify(cyData)}`);
 		return cyData;
 	});
-// https://gist.githubusercontent.com/maxkfranz/2c23fe9a23d0cc8d43af/raw
-const styleData = fetch('./circuit.txt')
-	.then(res => res.text());
 
-//Promise.all([graphData, styleData]).then(initCy);
-/*
-function initCy(then) {
-	var cy = window.cy = cytoscape({
-		container: document.getElementById('cytoscape-circuit'),
-		layout: { name: 'preset' },
-		elements: then[0].elements,
-		style: then[1],
-		motionBlur: true,
-		selectionType: 'single',
-		boxSelectionEnabled: false
-	});
-	mendData();
-} 
-*/
-
-
-/*
-
-function mendData(){
-	// because the source data doesn't connect nodes properly, use the cytoscape api to mend it:
-	cy.startBatch();
-	// put nodes in bins based on name
-	var nodes = cy.nodes();
-	var bin = {};
-	var metanames = [];
-	for( var i = 0; i < nodes.length; i++ ){
-		var node = nodes[i];
-		var name = node.data('station_name');
-		var nbin = bin[ name ] = bin[ name ] || [];
-
-		nbin.push( node );
-		
-		if( nbin.length === 2 ){
-			metanames.push( name );
-		}
-	}
-
-	// connect all nodes together with walking edges
-	for( var i = 0; i < metanames.length; i++ ){
-		var name = metanames[i];
-		var nbin = bin[ name ];
-
-		for( var j = 0; j < nbin.length; j++ ){
-			for( var k = j + 1; k < nbin.length; k++ ){
-				var nj = nbin[j];
-				var nk = nbin[k];
-				
-				cy.add({
-					group: 'edges',
-					data: {
-						source: nj.id(),
-						target: nk.id(),
-						is_walking: true
-					}
-				});
-				
-				//.css({
-			//    'line-color': 'yellow'
-				// });
-			}
-		}
-
-	}
-
-	cy.endBatch(); //.autolock( true );
-}
-*/
-	// store fetch functions in vars
-// load fetch functions in Promise
-// .then initiation function
-
-// init function
-
-/* data model
-"elements" : {
-    "nodes" : [ {
-      "data" : {
-        "id" : "8220",
-        "station_name" : "京成高砂",
-        "close_ymd" : "",
-        "lat" : 35.750932,
-        "lon" : 139.866875,
-        "post" : "",
-        "e_status" : 0,
-        "SUID" : 8220,
-        "station_g_cd" : 2300110,
-        "add" : "東京都葛飾区高砂五丁目28-1",
-        "line_cd" : 99340,
-        "selected" : false,
-        "open_ymd" : "",
-        "name" : "9934001",
-        "pref_name" : "東京都",
-        "shared_name" : "9934001",
-        "y" : -357509.32,
-        "x" : 1398668.75
-      },
-      "position" : {
-        "x" : 1398668.75,
-        "y" : -357509.32
-      },
-      "selected" : false
-*/
-/*
-var cy = cytoscape({
-
-  container: document.getElementById('cytoscape-circuit'), // container to render in
-
-  elements: [ // list of graph elements to start with
-    { // node a
-      data: { id: 'a' }
-    },
-    { // node b
-      data: { id: 'b' }
-    },
-    { // edge ab
-      data: { id: 'ab', source: 'a', target: 'b' }
-    }
-  ],
-
-  style: [ // the stylesheet for the graph
-    {
-      selector: 'node',
-      style: {
-        'background-color': '#666',
-        'label': 'data(id)'
-      }
-    },
-
-    {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle'
-      }
-    }
-  ],
-
-  layout: {
-    name: 'grid',
-    rows: 1
-  }
-
-});
-*/
 getStops();
+getRoutes();
+getEdges();
